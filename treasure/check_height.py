ed32cb5b8a926df45af0e65b908c04edcb3f3ba8
Dong Lieu <duongduydongtq@gmail.com>
2024-05-27 15:16:07 +0700

    ok

---
+++ treasure/check_height.py
@@ -0,0 +1,33 @@
+def eachHeight():
+    # file_path = '/Users/donglieu/52024/injective/cometbft-metrics/treasure/gen/block.csv'
+    file_path = "/Users/donglieu/52024/injective/cometbft-metrics/old_data/data5/block.csv"
+    return read_file(file_path)
+
+def read_file(path):
+    file = open(path, "r")
+
+    data = file.read().split("\n")
+
+    data = data[:-1]
+    for x in range(len(data)):
+        data[x] = data[x].split(",")
+     
+    return data
+
+def heightTimeOut():
+    eachHeigh = eachHeight()
+    height_timeout = []
+    max = 0
+    for x in eachHeigh:
+        # print(x)
+        if float(x[2]) > max:
+            max = float(x[2])
+
+        # if float(x[2]) > 3 and float(x[2]) < 5:
+        if float(x[2]) > 5:
+            height_timeout.append(x[0])
+    print(max)
+    return height_timeout
+# 1.520132042+0.002022702+2.937912053+0.088719586+0.637672203+0.05429426
+
+print(heightTimeOut())
---
+++ treasure/csv_reader.py
@@ -0,0 +1,59 @@
+import csv
+
+titleEachHeight = ['height', 'numRound', 'blockIntervalSeconds','numTx', 'blockSizeBytes', 'blockParts', 'blockGossipPartsReceived', 'quorumPrevoteDelay', 'fullPrevoteDelay', 'proposalReceiveCount', 'proposalCreateCount']
+titleEachVote = ['height', 'roundID', 'step', 'validatorsPower', 'missingValidatorsPowerPrevote']
+titleEachProposal = ['height', 'roundID', 'step', 'numblockParts', 'blockPartsReceived']
+titleEachTime = ['height', 'roundID', 'stepName', 'stepTime']
+titleEachP2P = ['height', 'roundID', 'step', 'fromPeer', 'toPeer', 'chID', 'msgType', 'size', 'rawByte']
+timeThreshold = 5.0
+
+def eachVote():
+    file_path = '/Users/donglieu/52024/injective/cometbft-metrics/treasure/gen/blockVoteStep.csv'
+
+    return read_file(file_path)
+
+def eachHeight():
+    file_path = '/Users/donglieu/52024/injective/cometbft-metrics/treasure/gen/block.csv'
+
+    return read_file(file_path)
+
+
+def eachProposal():
+    file_path = '/Users/donglieu/52024/injective/cometbft-metrics/treasure/gen/blockProposalStep.csv'
+
+    return read_file(file_path)
+
+def eachTimeStep():
+    file_path = '/Users/donglieu/52024/injective/cometbft-metrics/treasure/gen/blockOnlyTimeStep.csv'
+
+    return read_file(file_path)
+
+def eachP2P():
+    file_path = '/Users/donglieu/52024/injective/cometbft-metrics/treasure/gen/blockP2P.csv'
+
+    return read_file(file_path)
+
+def read_file(path):
+    file = open(path, "r")
+
+    data = file.read().split("\n")
+
+    data = data[:-1]
+    for x in range(len(data)):
+        data[x] = data[x].split(",")
+     
+    return data
+
+def heightTimeOut():
+    eachHeigh = eachHeight()
+    height_timeout = []
+    max = 0
+    for x in eachHeigh:
+        if float(x[2]) > max:
+            max = float(x[2])
+
+        if float(x[2]) > timeThreshold:
+            height_timeout.append(x[0])
+
+    # print(max)
+    return height_timeout
