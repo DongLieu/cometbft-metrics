d936c032871387d3ffdba7ee1c5f4bdf3ea6de5b
Dong Lieu <duongduydongtq@gmail.com>
2024-05-28 10:08:26 +0700

    ok

--- treasure/pro.py
+++ treasure/pro.py
@@ -42,32 +42,23 @@ valu2 = [i/len(heightTimeOuts) for i in blockPartsReceived.values()]
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Số lượng môn học
 num_subjects = len(stepName)
 
-# Tạo các vị trí cho các cột
 indices = np.arange(num_subjects)
 
-# Độ rộng của mỗi cột
 width = 0.35
 
-# Tạo biểu đồ
 fig, ax = plt.subplots()
 
-# Vẽ cột đầu tiên với màu xanh lá cây
 rects1 = ax.bar(indices - width/2, valu1, width, label='numblockParts', color='green')
 
-# Vẽ cột thứ hai với màu đỏ
 rects2 = ax.bar(indices + width/2, valu2, width, label='blockPartsReceived', color='red')
 
-# Thêm các nhãn, tiêu đề và chú giải
 ax.set_xticks(indices)
 ax.set_xticklabels(stepName)
 ax.legend()
 
-# Hiển thị số điểm trên mỗi cột
 ax.bar_label(rects1, padding=3)
 ax.bar_label(rects2, padding=3)
 
-# Hiển thị biểu đồ
 plt.show()
