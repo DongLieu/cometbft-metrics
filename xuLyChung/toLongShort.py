14cafaa50700c213effa5e4ffa034635002b8093
Dong Lieu <duongduydongtq@gmail.com>
2024-06-19 17:09:08 +0700

    ok

--- xuLyChung/toLongShort.py
+++ xuLyChung/toLongShort.py
@@ -7,29 +7,29 @@ def Chia_DataLong_DataShort(dirPath, heighttimeouts, heightContinues):
             continue
         if i[0] in heighttimeouts:
             write_file(dirPath + "/longBlock/block.csv", i)
-        
-        write_file(dirPath + "/shortBlock/block.csv", i)
+        else:
+            write_file(dirPath + "/shortBlock/block.csv", i)
         
     for i in read_file(dirPath + "/blockOnlyTimeStep.csv"):
         if i[0] in heightContinues:
             continue
         if i[0] in heighttimeouts:
             write_file(dirPath + "/longBlock/blockOnlyTimeStep.csv", i)
-        
-        write_file(dirPath + "/shortBlock/blockOnlyTimeStep.csv", i)
+        else:
+            write_file(dirPath + "/shortBlock/blockOnlyTimeStep.csv", i)
         
     for i in read_file(dirPath + "/blockProposalStep.csv"):
         if i[0] in heightContinues:
             continue
         if i[0] in heighttimeouts:
             write_file(dirPath + "/longBlock/blockProposalStep.csv", i)
-        
-        write_file(dirPath + "/shortBlock/blockProposalStep.csv", i)
+        else:
+            write_file(dirPath + "/shortBlock/blockProposalStep.csv", i)
 
     for i in read_file(dirPath + "/blockVoteStep.csv"):
         if i[0] in heightContinues:
             continue
         if i[0] in heighttimeouts:
             write_file(dirPath + "/longBlock/blockVoteStep.csv", i)
-        
-        write_file(dirPath + "/shortBlock/blockVoteStep.csv", i)
+        else:
+            write_file(dirPath + "/shortBlock/blockVoteStep.csv", i)
